fe0deb61852bcb300ce32267d18353e360265878
aksiooma <teijo.virta@gmail.com>
2024-01-30 16:30:17 +0200

    removed commonjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
 import react from '@vitejs/plugin-react'
 import { qrcode } from 'vite-plugin-qrcode';
 
@@ -9,7 +8,7 @@ export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
-  plugins: [react({}), qrcode(),  viteCommonjs()], 
+  plugins: [react({}), qrcode()], 
   define: { global: 'globalThis'},
   server: {
     port: parseInt(env.VITE_PORT),
