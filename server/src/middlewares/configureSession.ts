eacc444009ef661de0984d19012d5578d9cf238e
aksiooma <teijo.virta@gmail.com>
2025-03-05 18:06:13 +0200

    fix: remove custom domain from session config

--- server/src/middlewares/configureSession.ts
+++ server/src/middlewares/configureSession.ts
@@ -1,36 +1,35 @@
 import { Application } from 'express';
 import session from 'express-session';
 import pgSession from 'connect-pg-simple';
-import Database from '../database'; 
+import Database from '../database';
 
 const configureSession = (app: Application) => {
-    const secret = process.env.SESSION_SECRET;
-  
-    if (!secret) {
-      throw new Error('SESSION_SECRET is not set in the environment variables.');
-    }
-  
-    const pgPool = Database.getInstance();
-  
-    app.use(
-      session({
-        store: new (pgSession(session))({
-          pool: pgPool,
-          createTableIfMissing: true
-        }),
-        secret,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-          maxAge: 24 * 60 * 60 * 1000,
-          secure: true, // Muutettu
-          httpOnly: true,
-          sameSite: 'none', // Muutettu 'strict' -> 'none'
-          domain: '.onrender.com' // Voidaan poistaa jos ei toimi
-        },
-        proxy: true // Lisätty
-      })
-    );
-  };
-  
-  export default configureSession;
+  const secret = process.env.SESSION_SECRET;
+
+  if (!secret) {
+    throw new Error('SESSION_SECRET is not set in the environment variables.');
+  }
+
+  const pgPool = Database.getInstance();
+
+  app.use(
+    session({
+      store: new (pgSession(session))({
+        pool: pgPool,
+        createTableIfMissing: true
+      }),
+      secret,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: true,
+        httpOnly: true,
+        sameSite: 'none'
+      },
+      proxy: true
+    })
+  );
+};
+
+export default configureSession;
